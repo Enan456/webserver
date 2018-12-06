0e6e27972b61187eb891c27fb50c15ecbf4ed9af
Ethan <ethan200019@gmail.com>
2018-12-05 19:41:32 -0600

    Service Worker Fix 3
    - Google devs don't fail me now

--- assets/js/sg.js
+++ assets/js/sg.js
@@ -1,10 +1,39 @@
 var submit2 = document.getElementById('submit');
 
-        if('serviceWorker' in navigator) {
-navigator.serviceWorker
-       .register('assets/js/sg.js')
-       .then(function() { console.log("Service Worker Registered"); });
+if ('serviceWorker' in navigator) {
+window.addEventListener('load', function() {
+navigator.serviceWorker.register('assets/js/sg.js').then(function(registration) {
+// Registration was successful
+console.log('ServiceWorker registration successful with scope: ', registration.scope);
+}, function(err) {
+// registration failed :(
+console.log('ServiceWorker registration failed: ', err);
+});
+});
 }
+self.addEventListener('install', function(event) {
+  // Perform install steps
+});
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/assets/js/bootstrap.min.js',
+  '/css/materialize.css',
+  '/assets/js/sg.js',
+  '/assets/js/index.js',
+  '/assets/js/bootstrap.min.js'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
         var config = {
             apiKey: "AIzaSyAGjIhyS5eGmUvZysE46CuWFjeX0T-ykWA",
             authDomain: "scoutgear-2018.firebaseapp.com",
