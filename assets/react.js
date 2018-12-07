83b5442db80463f1fe36067f30a62a9897aa03d4
Lucas <origamimaster62003@gmail.com>
2018-12-06 19:42:49 -0600

    React V5.

---
+++ assets/react.js
@@ -0,0 +1,89 @@
+ReactDOM.render(
+    <input id="yourname" type="text" class="validate"></input>
+    <label for="yourname">Name</label>
+    document.getElementById('name')
+  );
+
+  ReactDOM.render(
+    <input id="match" type="text" class="validate"></input>
+    <label for="match">Match</label>
+    document.getElementById('match')
+  );
+
+  ReactDOM.render(
+    <input id="yourteam" type="text" class="validate"></input>
+    <label for="yourteam">Team You Are In</label>
+    document.getElementById('your-team')
+  );
+
+  ReactDOM.render(
+    <input id="scoutteam" type="text" class="validate"></input>
+    <label for="scoutteam">Team You Are Scouting</label>
+    document.getElementById('scouted-team')
+  );
+
+  ReactDOM.render(
+    <input id="regional" type="text" class="validate"></input>
+    <label for="regional">Regional Code</label>
+    document.getElementById('regoinal-code')
+  );
+
+  ReactDOM.render(
+    <input type="checkbox" class="filled-in" value="line" id="auto-0"></input>
+    <span>Line Crossed</span>
+    document.getElementById('crossed-line')
+  );
+
+  ReactDOM.render(
+    <input type="checkbox" class="filled-in" value="block-auto" id="auto-1"></input>
+    <span>Block Placed On Switch</span>
+    document.getElementById('switch-block')
+  );
+
+  ReactDOM.render(
+    <input type="checkbox" class="filled-in" value="block2-auto" id="auto-3"></input>
+    <span>Block Placed On Scale</span>
+    document.getElementById('scale-block')
+  );
+
+  ReactDOM.render(
+    <input type="checkbox" class="filled-in" value="penalty-auto" id="auto-2"></input>
+    <span>Penalty</span>
+    document.getElementById('penalty')
+  );
+
+  ReactDOM.render(
+    <input type="checkbox" class="filled-in" value="forklift" id="teleop-0"></input>
+    <span>Forklift</span>
+    document.getElementById('forklift')
+  );
+
+  ReactDOM.render(
+    <input type="checkbox" class="filled-in" value="defense" id="teleop-1"></input>
+    <span>Defense</span>
+    document.getElementById('defense')
+  );
+
+  ReactDOM.render(
+    <input id="pcubes" type="text" class="validate"></input>
+    <label for="pcubes">Portal Power Cubes</label>
+    document.getElementById('portal-power')
+  );
+
+  ReactDOM.render(
+    <input id="faswitch" type="text" class="validate"></input>
+    <label for="faswitch">Cubes In Friendly Alliance Switch</label>
+    document.getElementById('freindly-switch-cubes')
+  );
+
+  ReactDOM.render(
+    <input id="scale" type="text" class="validate"></input>
+    <label for="scale">Cubes In Scale</label>
+    document.getElementById('scale-cubes')
+  );
+
+  ReactDOM.render(
+    <input id="opswitch" type="text" class="validate"></input>
+    <label for="opswitch">Cubes In Opposing Alliance Switch</label>
+    document.getElementById('enemy-switch-cubes')
+  );
