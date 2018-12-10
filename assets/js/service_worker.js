cdf6ee1d559140b19a5ee39b098d7f95ec2329ee
Ethan <ethan200019@gmail.com>
2018-12-10 10:46:36 -0600

    A Variable Fix
    - Makes the CACHE_NAME uniform

--- assets/js/service_worker.js
+++ assets/js/service_worker.js
@@ -31,7 +31,7 @@ self.addEventListener('install', function (event) {
         })
     );
 });
-let CACHE_NAME = 'sw-v1'
+let CACHE_NAME = 'my-site-cache-v1'
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
