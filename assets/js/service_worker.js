69a67d130a4682ddfa2567ed48e9ae6f9f4d47a2
Ethan <ethan200019@gmail.com>
2018-12-10 18:41:02 -0600

    HTML Cache Source
    - Changed source to sg.html

--- assets/js/service_worker.js
+++ assets/js/service_worker.js
@@ -66,7 +66,7 @@ let CACHE_NAME = 'sw-v1'
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-    .then(cache => cache.addAll('./404.html'))
+    .then(cache => cache.addAll('sg.html'))
   )
 })
 self.addEventListener('fetch', (event) => {
