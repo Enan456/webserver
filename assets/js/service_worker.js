183d08c0848f84821b17112235b236591d722a15
Ethan <ethan200019@gmail.com>
2018-12-10 18:35:58 -0600

    Another Day, Another Service Worker
    - Should work offline

--- assets/js/service_worker.js
+++ assets/js/service_worker.js
@@ -1,4 +1,4 @@
-if ('serviceWorker' in navigator) {
+/*if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
         navigator.serviceWorker.register('assets/js/sg.js').then(function (registration) {
             // Registration was successful
@@ -47,6 +47,37 @@ self.addEventListener('fetch', (event) => {
           .then((response) => {
             let cacheCopy = response.clone()
             caches.open(CACHE_CHECK)
+              .then(cache => cache.put(event.request, cacheCopy))
+            return response;
+          })
+          .catch(() => caches.match(offlinePage));
+        return cached || networked;
+      })
+    )
+  }
+  return;
+});
+*/
+
+// New Service serviceWorker
+
+
+let CACHE_NAME = 'sw-v1'
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(cache => cache.addAll('./404.html'))
+  )
+})
+self.addEventListener('fetch', (event) => {
+  if (event.request.method === 'GET') {
+    event.respondWith(
+      caches.match(event.request)
+      .then((cached) => {
+        var networked = fetch(event.request)
+          .then((response) => {
+            let cacheCopy = response.clone()
+            caches.open(CACHE_NAME)
               .then(cache => cache.put(event.request, cacheCopy))
             return response;
           })
