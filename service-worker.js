7efae8f2ff8109df9b9c1904ce846d5dcda02557
Ethan <ethan200019@gmail.com>
2019-03-20 10:41:57 -0500

    Workbox 7
    - Routed critical css and js
    - Will cache images and icons

--- service-worker.js
+++ service-worker.js
@@ -9,6 +9,44 @@ if (workbox) {
 }
 
 workbox.routing.registerRoute(
-    /\.js$/,
-    …
+  'assets/js/sg.js',
+  handler
   );
+workbox.routing.registerRoute(
+  'assets/js/bootstrap.min.js',
+  handler
+  );
+workbox.routing.registerRoute(
+  'assets/js/materialize.min.js',
+  handler
+  );
+workbox.routing.registerRoute(
+  'assets/css/sg.css',
+  handler
+  );
+workbox.routing.registerRoute(
+  'assets/css/sgdata.css',
+  handler
+  );
+workbox.routing.registerRoute(
+  'assets/css/main.css',
+  handler
+  );
+
+  workbox.routing.registerRoute(
+  // Cache image files.
+  /\.(?:png|jpg|jpeg|svg|gif)$/,
+  // Use the cache if it's available.
+  new workbox.strategies.CacheFirst({
+    // Use a custom cache name.
+    cacheName: 'image-cache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        // Cache only 20 images.
+        maxEntries: 20,
+        // Cache for a maximum of a week.
+        maxAgeSeconds: 7 * 24 * 60 * 60,
+      })
+    ],
+  })
+);
