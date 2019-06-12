5a0c2e15c1a00034a91fbeecba2868b1d63e7244
Enan <enan332@gmail.com>
2019-06-12 16:53:43 -0700

    clear house and prep

--- assets/react/button.js
+++
@@ -1,7 +0,0 @@
-ReactDOM.render(
-    <React.Fragment>
-    <input type="button" class="filled-in" value="penalty-auto" id="auto-2"></input>
-    <span>button</span>
-    document.getElementById('button')
-    </React.Fragment>
-  );
