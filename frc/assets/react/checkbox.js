8440846b3644762d1377c9c50c175bbc006d21fd
enan456 <enan332@gmail.com>
2020-05-20 07:30:25 -0500

    migrated

--- frc/assets/react/checkbox.js
+++
@@ -1,7 +0,0 @@
-ReactDOM.render(
-    <React.Fragment>
-    <input type="checkbox" class="filled-in" value="penalty-auto" id="auto-2"></input>
-    <span>checkbox</span>
-    document.getElementById('checkbox')
-    </React.Fragment>
-  );
